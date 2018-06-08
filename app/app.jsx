bdf191f5a7d35719a55be2530a313d62243a511a
Alexshumansky <shumaxer.x@mail.ru>
2018-06-08 13:58:50 +0300

    basic structure

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+import {render} from 'react-dom';
+import React from 'react';
+
+import Game from './components/Game.jsx';
+
+render(
+	<Game />,
+	document.getElementById("app")
+);
---
+++ app/components/Game.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Game extends Component {
+	render() {
+		return <h1>Welcome to game</h1>
+	}
+}
