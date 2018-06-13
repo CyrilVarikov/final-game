1d9007644ec62372901f2a9d6cc16f868ab27371
Alexshumansky <shumaxer.x@mail.ru>
2018-06-13 14:42:10 +0300

    css for Registration

--- app/components/Registration.jsx
+++ app/components/Registration.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ChooseHero from './ChooseHero.jsx';
+
 import '../css/registration.css';
 
 export default class Registration extends Component {
@@ -26,13 +27,9 @@ export default class Registration extends Component {
 		if (!this.state.hidden) {
 			return (
 				<form onSubmit = {this.handleSubmit}>
-					<label>Name</label><br/>
-					<input type="text" ref="name"/><br/>
-	
-					<label>Password</label><br/>
-					<input type="password" ref="password"/><br/>
-	
-					<input type="submit" value="Registration"/>
+					<input type="text" ref="name" placeholder='Name*' maxLength='16' minLength='3'/><br/>
+					<input type="password" ref="password" placeholder='Password*' maxLength='16' minLength='4'/><br/>
+					<input type="submit" value="Sign in"/>
 				</form>
 			);
 		} else {
